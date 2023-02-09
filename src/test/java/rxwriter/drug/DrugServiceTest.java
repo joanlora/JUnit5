cfeeea0f14546d29596176f0b22d5fd9908f343c
Joan Lora <joan.lora@outlook.com>
2023-02-09 14:22:18 -0800

    Added throwsExceptionOnEmptyStartsWith

--- src/test/java/rxwriter/drug/DrugServiceTest.java
+++ src/test/java/rxwriter/drug/DrugServiceTest.java
@@ -9,12 +9,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class DrugServiceTest {
 
     @Test
-    void findDrugsStartingWith() {
+    void drugsAreReturnedSorted() {
         DrugService service = new DrugService();
         List<DispensableDrug> foundDrugs = service.findDrugsStartingWith("as");
         assertNotNull(foundDrugs);
-        assertEquals(2,foundDrugs.size());
+        assertEquals(2, foundDrugs.size());
         assertEquals("asmanex", foundDrugs.get(0).drugName());
         assertEquals("aspirin", foundDrugs.get(1).drugName());
+    }
+
+    @Test
+    void throwsExceptionOnEmptyStartsWith() {
+        DrugService service = new DrugService();
+        Exception thrown = assertThrows(IllegalArgumentException.class,
+                () -> service.findDrugsStartingWith(" "));
+        System.out.println(thrown.getMessage());
     }
 }
